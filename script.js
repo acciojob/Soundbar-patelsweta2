eb759f33cfc92709337662d06826fc705c84683e
patelsweta2 <80040744+patelsweta2@users.noreply.github.com>
2024-02-23 16:02:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong', 'stop'];
+
+sounds.forEach(sound => {
+	const btn = document.createElement('button');
+	btn.classList.add('btn');
+	btn.innerText = sound;
+	btn.addEventListener('click', () => {
+		stopSongs();
+		document.getElementById(sound).play();
+	});
+	document.getElementById('buttons').appendChild(btn);
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound);
+        song.pause();
+        song.currentTime = 0;
+    })
+}
+
